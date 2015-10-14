23f087a45d4dcd0f23b8b76db57a82132af79b9e
William Willing <william@williamwilling.com>
2015-10-14 09:27:45 +0200

    You can now replace values with a patch.

--- Lisa.Common.WebApi/Patcher.cs
+++ Lisa.Common.WebApi/Patcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Lisa.Common.WebApi
 {
@@ -6,6 +8,32 @@ namespace Lisa.Common.WebApi
     {
         public static void Apply(IEnumerable<Patch> patches, object obj)
         {
+            foreach (var patch in patches)
+            {
+                switch (patch.Action.ToLower())
+                {
+                    case "replace":
+                        ApplyReplace(patch, obj);
+                        break;
+                }
+            }
+        }
+
+        private static void ApplyReplace(Patch patch, object obj)
+        {
+            var property = GetProperty(obj, patch.Field);
+            SetProperty(obj, property, patch.Value);
+        }
+
+        private static PropertyInfo GetProperty(object obj, string field)
+        {
+            var type = obj.GetType();
+            return type.GetProperty(field, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        private static void SetProperty(object obj, PropertyInfo property, JToken value)
+        {
+            property.SetValue(obj, value?.ToObject(property.PropertyType));
         }
     }
 }
