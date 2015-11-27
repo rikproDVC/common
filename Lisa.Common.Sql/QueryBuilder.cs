560dd2f0ddab01089383f963b9f70c73027d2544
William Willing <william@williamwilling.com>
2015-11-27 15:25:21 +0100

    Created a QueryBuilder that can add parameters to a SQL query.

---
+++ Lisa.Common.Sql/QueryBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Lisa.Common.Sql
+{
+    public static class QueryBuilder
+    {
+        public static string Build(string query, object parameters = null)
+        {
+            if (parameters != null)
+            {
+                foreach (var property in parameters.GetType().GetProperties())
+                {
+                    var name = string.Format("'@{0}'", property.Name);
+                    var value = string.Format("'{0}'", property.GetValue(parameters));
+                    query = query.Replace(name, value);
+
+                    name = string.Format("@{0}", property.Name);
+                    value = string.Format("'{0}'", property.GetValue(parameters));
+                    query = query.Replace(name, value);
+                }
+            }
+
+            return query;
+        }
+    }
+}
---
+++ Lisa.Common.UnitTests/Sql/QueryBuilderTest.cs
@@ -0,0 +1,45 @@
+﻿using Lisa.Common.Sql;
+using Xunit;
+
+namespace Lisa.Common.UnitTests.Sql
+{
+    public class QueryBuilderTest
+    {
+        [Fact]
+        public void ItReturnsQueriesWithoutParametersUnchanged()
+        {
+            string query = "SELECT * FROM Planets";
+
+            string result = QueryBuilder.Build(query);
+            Assert.Equal("SELECT * FROM Planets", result);
+        }
+
+        [Fact]
+        public void ItReplacesValueParameters()
+        {
+            string query = "SELECT * FROM Planets WHERE MoonCount=@MoonCount AND Star=@Star";
+            object parameters = new
+            {
+                MoonCount = 2,
+                Star = "Sol"
+            };
+
+            string result = QueryBuilder.Build(query, parameters);
+            Assert.Equal("SELECT * FROM Planets WHERE MoonCount='2' AND Star='Sol'", result);
+        }
+
+        [Fact]
+        public void ItDoesNotAddUnnecessaryQuotes()
+        {
+            string query = "SELECT * FROM Planets WHERE MoonCount='@MoonCount' AND Star='@Star'";
+            object parameters = new
+            {
+                MoonCount = 2,
+                Star = "Sol"
+            };
+
+            string result = QueryBuilder.Build(query, parameters);
+            Assert.Equal("SELECT * FROM Planets WHERE MoonCount='2' AND Star='Sol'", result);
+        }
+    }
+}
